7d6fec4d07336d5e0f89a1bdd5a78a44462c2c6a
wh75er <wh75er@gmail.com>
2020-02-03 19:10:15 +0300

    add type determinator function

---
+++ fs_determ.c
@@ -0,0 +1,29 @@
+#include "fs_determ.h"
+
+void get_bd_fs_type(char* path, char* type)
+{
+    blkid_probe pr;
+    pr = blkid_new_probe_from_filename("/dev/sda5");
+    if(!pr) {
+        printf("Err: Failed to open device\n");
+        return;
+    }
+
+    const char* _type;
+    blkid_do_probe(pr);
+    blkid_probe_lookup_value(pr, "TYPE", &_type, NULL);
+
+    strcpy(type, _type);
+
+    blkid_free_probe(pr);
+}
+
+int main()
+{
+    char* path = "/dev/sdc1";
+    char type[50] = ""; 
+
+    get_bd_fs_type(path, type);
+
+    printf("FILE: %s has type %s\n", path, type);
+}
---
+++ fs_determ.h
@@ -0,0 +1,8 @@
+#ifndef __FS_DETERM__
+#define __FS_DETERM__
+
+#include <blkid/blkid.h>
+#include <string.h>
+#include <stdio.h>
+
+#endif
